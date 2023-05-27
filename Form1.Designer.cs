ad0f09821375f8183960f5b172fdf55e3f0ccb54
Sandro <fernando.meza.perales@gmail.com>
2023-05-27 13:36:06 -0500

    Interfaz grafica de dueños y funcionalidad lista

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -32,23 +32,23 @@
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.tbpgs_mascotas = new System.Windows.Forms.TabPage();
             this.tbpg_dueños = new System.Windows.Forms.TabPage();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.Agregar = new System.Windows.Forms.Button();
+            this.txtbox_telefono = new System.Windows.Forms.TextBox();
+            this.txtbox_correo = new System.Windows.Forms.TextBox();
+            this.txtbox_nombre = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dgvDueños = new System.Windows.Forms.DataGridView();
             this.nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.direccion = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.telefono = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.tabcontrol = new System.Windows.Forms.TabControl();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
             this.tbpg_dueños.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
-            this.tabcontrol.SuspendLayout();
             this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDueños)).BeginInit();
+            this.tabcontrol.SuspendLayout();
             this.SuspendLayout();
             // 
             // contextMenuStrip1
@@ -62,7 +62,7 @@
             this.tbpgs_mascotas.Location = new System.Drawing.Point(4, 29);
             this.tbpgs_mascotas.Name = "tbpgs_mascotas";
             this.tbpgs_mascotas.Padding = new System.Windows.Forms.Padding(3);
-            this.tbpgs_mascotas.Size = new System.Drawing.Size(968, 587);
+            this.tbpgs_mascotas.Size = new System.Drawing.Size(968, 492);
             this.tbpgs_mascotas.TabIndex = 1;
             this.tbpgs_mascotas.Text = "Mascotas";
             this.tbpgs_mascotas.UseVisualStyleBackColor = true;
@@ -71,7 +71,7 @@
             // tbpg_dueños
             // 
             this.tbpg_dueños.Controls.Add(this.groupBox1);
-            this.tbpg_dueños.Controls.Add(this.dataGridView1);
+            this.tbpg_dueños.Controls.Add(this.dgvDueños);
             this.tbpg_dueños.Location = new System.Drawing.Point(4, 29);
             this.tbpg_dueños.Name = "tbpg_dueños";
             this.tbpg_dueños.Padding = new System.Windows.Forms.Padding(3);
@@ -80,25 +80,103 @@
             this.tbpg_dueños.Text = "Dueños";
             this.tbpg_dueños.UseVisualStyleBackColor = true;
             // 
-            // dataGridView1
+            // groupBox1
             // 
-            this.dataGridView1.AllowUserToAddRows = false;
-            this.dataGridView1.AllowUserToDeleteRows = false;
-            this.dataGridView1.AllowUserToResizeColumns = false;
-            this.dataGridView1.AllowUserToResizeRows = false;
-            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.groupBox1.Controls.Add(this.Agregar);
+            this.groupBox1.Controls.Add(this.txtbox_telefono);
+            this.groupBox1.Controls.Add(this.txtbox_correo);
+            this.groupBox1.Controls.Add(this.txtbox_nombre);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(593, 108);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(360, 277);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Agregar Dueño";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // Agregar
+            // 
+            this.Agregar.Location = new System.Drawing.Point(11, 201);
+            this.Agregar.Name = "Agregar";
+            this.Agregar.Size = new System.Drawing.Size(341, 44);
+            this.Agregar.TabIndex = 8;
+            this.Agregar.Text = "Agregar";
+            this.Agregar.UseVisualStyleBackColor = true;
+            this.Agregar.Click += new System.EventHandler(this.Agregar_Click);
+            // 
+            // txtbox_telefono
+            // 
+            this.txtbox_telefono.Location = new System.Drawing.Point(84, 143);
+            this.txtbox_telefono.Name = "txtbox_telefono";
+            this.txtbox_telefono.Size = new System.Drawing.Size(268, 27);
+            this.txtbox_telefono.TabIndex = 7;
+            this.txtbox_telefono.TextChanged += new System.EventHandler(this.txtbox_telefono_TextChanged);
+            this.txtbox_telefono.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtbox_telefono_KeyPress);
+            // 
+            // txtbox_correo
+            // 
+            this.txtbox_correo.Location = new System.Drawing.Point(84, 98);
+            this.txtbox_correo.Name = "txtbox_correo";
+            this.txtbox_correo.Size = new System.Drawing.Size(268, 27);
+            this.txtbox_correo.TabIndex = 6;
+            // 
+            // txtbox_nombre
+            // 
+            this.txtbox_nombre.Location = new System.Drawing.Point(84, 50);
+            this.txtbox_nombre.Name = "txtbox_nombre";
+            this.txtbox_nombre.Size = new System.Drawing.Size(268, 27);
+            this.txtbox_nombre.TabIndex = 5;
+            this.txtbox_nombre.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtbox_nombre_KeyPress);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(11, 146);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Telefono";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Correo";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Nombre";
+            // 
+            // dgvDueños
+            // 
+            this.dgvDueños.AllowUserToAddRows = false;
+            this.dgvDueños.AllowUserToDeleteRows = false;
+            this.dgvDueños.AllowUserToResizeColumns = false;
+            this.dgvDueños.AllowUserToResizeRows = false;
+            this.dgvDueños.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvDueños.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDueños.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.nombre,
             this.direccion,
             this.telefono});
-            this.dataGridView1.Location = new System.Drawing.Point(6, 6);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.ReadOnly = true;
-            this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.RowTemplate.Height = 29;
-            this.dataGridView1.Size = new System.Drawing.Size(567, 476);
-            this.dataGridView1.TabIndex = 0;
+            this.dgvDueños.Location = new System.Drawing.Point(6, 6);
+            this.dgvDueños.Name = "dgvDueños";
+            this.dgvDueños.ReadOnly = true;
+            this.dgvDueños.RowHeadersWidth = 51;
+            this.dgvDueños.RowTemplate.Height = 29;
+            this.dgvDueños.Size = new System.Drawing.Size(567, 476);
+            this.dgvDueños.TabIndex = 0;
             // 
             // nombre
             // 
@@ -131,80 +209,6 @@
             this.tabcontrol.Size = new System.Drawing.Size(976, 525);
             this.tabcontrol.TabIndex = 1;
             // 
-            // groupBox1
-            // 
-            this.groupBox1.Controls.Add(this.button1);
-            this.groupBox1.Controls.Add(this.textBox3);
-            this.groupBox1.Controls.Add(this.textBox2);
-            this.groupBox1.Controls.Add(this.textBox1);
-            this.groupBox1.Controls.Add(this.label3);
-            this.groupBox1.Controls.Add(this.label2);
-            this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Location = new System.Drawing.Point(593, 108);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(360, 277);
-            this.groupBox1.TabIndex = 1;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Agregar Dueño";
-            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(11, 53);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(64, 20);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Nombre";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(11, 101);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(54, 20);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "Correo";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(11, 146);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(67, 20);
-            this.label3.TabIndex = 4;
-            this.label3.Text = "Telefono";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(84, 50);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(268, 27);
-            this.textBox1.TabIndex = 5;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(84, 98);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(268, 27);
-            this.textBox2.TabIndex = 6;
-            // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(84, 143);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(268, 27);
-            this.textBox3.TabIndex = 7;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(11, 201);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(341, 44);
-            this.button1.TabIndex = 8;
-            this.button1.Text = "Agregar";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
@@ -214,10 +218,10 @@
             this.Name = "Form1";
             this.Text = "Veterinaria";
             this.tbpg_dueños.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
-            this.tabcontrol.ResumeLayout(false);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDueños)).EndInit();
+            this.tabcontrol.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -228,15 +232,15 @@
         private TabPage tbpgs_mascotas;
         private TabPage tbpg_dueños;
         private TabControl tabcontrol;
-        private DataGridView dataGridView1;
+        private DataGridView dgvDueños;
         private DataGridViewTextBoxColumn nombre;
         private DataGridViewTextBoxColumn direccion;
         private DataGridViewTextBoxColumn telefono;
         private GroupBox groupBox1;
-        private Button button1;
-        private TextBox textBox3;
-        private TextBox textBox2;
-        private TextBox textBox1;
+        private Button Agregar;
+        private TextBox txtbox_telefono;
+        private TextBox txtbox_correo;
+        private TextBox txtbox_nombre;
         private Label label3;
         private Label label2;
         private Label label1;
--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,37 @@ namespace Veterinaria
         {
 
         }
+
+        private void Agregar_Click(object sender, EventArgs e)
+        {
+            int fila = dgvDueños.Rows.Add();
+            string Nombre = Convert.ToString(txtbox_nombre.Text).ToUpper();
+            string Correo = Convert.ToString(txtbox_correo.Text).ToUpper();
+            string Telefono = Convert.ToString(txtbox_telefono.Text).ToUpper();
+            dgvDueños.Rows[fila].Cells[0].Value = Nombre;
+            dgvDueños.Rows[fila].Cells[1].Value = Correo;
+            dgvDueños.Rows[fila].Cells[2].Value = Telefono;
+        }
+
+        private void txtbox_telefono_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtbox_telefono_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!Char.IsDigit(e.KeyChar) && e.KeyChar != 8)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void txtbox_nombre_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsLetter(e.KeyChar) && e.KeyChar != 8)
+            {
+                e.Handled = true;
+            }
+        }
     }
 }
